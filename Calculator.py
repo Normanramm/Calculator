6c1711037092df26b843ef7be48e582c7e40f214
Vladislav <144459367+Normanramm@users.noreply.github.com>
2024-08-12 20:46:17 +1000

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,24 +1,24 @@
-class CalculatorClass:
+class CalculatorClass:  # Класс для калькулятора
     def plus(self):
         a = int(input())
         b = int(input())
-        return a + b
+        return f'{a} + {b} = {a + b}'
 
     def minus(self):
         a = int(input())
         b = int(input())
-        return a - b
+        return f'{a} - {b} = {a - b}'
 
     def ymnojenie(self):
         a = int(input())
         b = int(input())
-        return a * b
+        return f'{a} * {b} = {a * b}'
 
     def delenie(self):
         try:
             a = int(input())
             b = int(input())
-            return a / b
+            return f'{a} / {b} = {a / b}'
         except ZeroDivisionError:
             print('На ноль делить нельзя!')
 
@@ -26,20 +26,20 @@ class CalculatorClass:
         try:
             a = int(input())
             b = int(input())
-            return a % b
+            return f'{a} % {b} = {a % b}'
         except ZeroDivisionError:
             print('На ноль делить нельзя!')
 
     def vozvedei_vstepen(self):
         a = int(input())
         b = int(input())
-        return a ** b
+        return f'{a} ** {b} = {a ** b}'
 
     def celochislennoe_delenie(self):
         try:
             a = int(input())
             b = int(input())
-            return a // b
+            return f'{a} // {b} = {a // b}'
         except ZeroDivisionError:
             print('На ноль делить нельзя!')
 
@@ -47,7 +47,19 @@ class CalculatorClass:
 calculator = CalculatorClass()
 
 
-def cal():  # Калькулятор(тут дописать другие математические операции)
+class MathematicalClass:  # Класс для математических функций
+    def tabl(self):
+        for i in range(1, 10):
+            print('-' * 34)
+            for y in range(1, 10):
+                print(i * y, end="\t")
+            print()
+
+
+mathematical = MathematicalClass()
+
+
+def cal():  # Калькулятор
     flag = input('Выберите операцию: +, -,  *, /, %, **, //: ')
     if flag == '+':
         print(calculator.plus())
@@ -67,17 +79,29 @@ def cal():  # Калькулятор(тут дописать другие мат
         print('Неправильный выбор операции!')
 
 
+def mathemat():  # Математические функции
+    flag_2 = input('''Математические функции:
+    1 - Таблица умножения
+    2 - что то еще
+    3 - что то еще''')
+    if flag_2 == '1':
+        print(mathematical.tabl())
+
+    else:
+        print('Неправильный выбор операции!')
+
+
 def vibor():  # Тут выбор на разные функции(далее будут добавлены другие программы)
     vibor_prog = input('''Выберите функционал: 
     1 - Калькулятор
     2 - Математические функции
     3 - Скачать весь ЮТУБ
-    4 - Скорость интернета'''
+    4 - Скорость интернета '''
                        )
     if vibor_prog == '1':
         cal()
     elif vibor_prog == '2':
-        pass
+        mathemat()
     elif vibor_prog == '3':
         pass
     elif vibor_prog == '4':
@@ -89,10 +113,10 @@ vibor()
 
 
 while True:  # Возврат к выбору функционала
-    flag_2 = input('Еще раз: да / нет: ')
-    if flag_2 == 'да':
+    flag_3 = input('Еще раз: да / нет: ')
+    if flag_3 == 'да':
         cal()
-    elif flag_2 == 'нет':
+    elif flag_3 == 'нет':
         vibor()
     else:
         break
